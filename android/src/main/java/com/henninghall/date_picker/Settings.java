86356c583beed7914b02d9f835294af5e524706b
adrianchinghc <adrian@upstackstudio.com>
2020-04-09 22:10:10 +0800

    Remove example and am pm function

--- android/src/main/java/com/henninghall/date_picker/Settings.java
+++ android/src/main/java/com/henninghall/date_picker/Settings.java
@@ -6,7 +6,7 @@ import android.text.format.DateFormat;
 public class Settings {
 
     public static boolean usesAmPm (){
-        return !DateFormat.is24HourFormat(DatePickerManager.context);
+        return false;
     }
 
 }
